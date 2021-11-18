d28b8808b55d641c68feefe21eab2d6f6b77a546
Alex <vuyanalex@gmail.com>
2021-11-19 00:58:34 +0300

    add feedback comp

--- frontend/src/components/UI/button/FeedbackBtn/Btn.js
+++ frontend/src/components/UI/button/FeedbackBtn/Btn.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import styles from './PromoBtn.module.scss';
+import styles from './Btn.module.scss';
 
 
-export const Btn = ({ children, props }) => {
+export const Btn = ({ children, ...props }) => {
     return (
         <button {...props} className={styles.btn}>
             <span className={styles.btn__text}>
